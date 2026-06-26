ba3510fa95669a8974d77677093ef21e51c00743
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2026-06-26 13:55:56 +0800

    commit

---
+++ LogiTrack/Models/InventoryItem.cs
@@ -0,0 +1,8 @@
+public class InventoryItem
+{
+    public int ItemId { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public string Location { get; set; }
+}
+
