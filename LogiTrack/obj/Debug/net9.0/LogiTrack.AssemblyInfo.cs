04b8f08f8a3eb6fbf20b03a16d9a4a3b439771dd
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2026-07-22 13:30:12 +0800

    commit

--- LogiTrack/obj/Debug/net9.0/LogiTrack.AssemblyInfo.cs
+++ LogiTrack/obj/Debug/net9.0/LogiTrack.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LogiTrack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a306af70ccc42498647bafba9e75b562ea839a06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ec8e843625e4eedda1d35ba1321e0bb0d7bd077")]
 [assembly: System.Reflection.AssemblyProductAttribute("LogiTrack")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LogiTrack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
