e6a3c2068ef90544aa2958ee39e083e2f21d4b23
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2026-08-31 19:51:45 +0800

    commit

--- LogiTrack/Program.cs
+++ LogiTrack/Program.cs
@@ -8,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.HttpsPort = 7077;
+    options.RedirectStatusCode = StatusCodes.Status307TemporaryRedirect;
+});
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=logitrack.db";
 
